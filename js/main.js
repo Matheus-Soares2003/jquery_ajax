1730e1a21cb84db1e0f3235c678a2f2e1349c668
Matheus-Soares2003 <matheusstcabral@gmail.com>
2022-03-30 09:39:48 -0300

    Introdução do Bootstrap

--- js/main.js
+++ js/main.js
@@ -5,14 +5,6 @@ function consultaCep(){
         type: "GET",
         success: function(response){
             console.log(response)
-            $("#cidade").html(response.localidade)
-            //document.getElementById("cidade").innerHTML = response.localidade
-            $("#uf").html(`- ${response.uf}`)
-            //document.getElementById("uf").innerHTML = `- ${response.uf}`
-            $("#bairro").html(response.bairro)
-            //document.getElementById("bairro").innerHTML = response.bairro
-            $("#logradouro").html(response.logradouro)
-            //document.getElementById("logradouro").innerHTML = response.logradouro
         }
     })
 }
